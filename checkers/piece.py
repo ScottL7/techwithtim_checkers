a6f4f0f9a2f8b84a9f8e4a2ef6e81e06f2418f13
Scott Leitstein <scott@compuintel.com>
2020-09-15 22:17:27 -0400

    Initial Commit  - Added highlight to selected piece  - if player selects a blank square or opponent's piece after selecting a piece it will be unselected.

---
+++ checkers/piece.py
@@ -0,0 +1,46 @@
+import pygame
+from .constants import RED, SQUARE_SIZE, GREY, CROWN, GREEN
+
+
+class Piece:
+    PADDING = SQUARE_SIZE * 0.15  # 15% of the size of the square
+    OUTLINE = 2
+
+    def __init__(self, row, col, color):
+        self.row = row
+        self.col = col
+        self.color = color
+        self.king = False
+        self.selected = False
+
+        self.x = 0
+        self.y = 0
+        self.calc_pos()
+
+    def calc_pos(self):
+        self.x = SQUARE_SIZE * self.col + SQUARE_SIZE // 2
+        self.y = SQUARE_SIZE * self.row + SQUARE_SIZE // 2
+
+    def make_king(self):
+        self.king = True
+
+    def set_selected(self, selected):
+        self.selected = selected
+
+    def draw(self, win):
+        radius = SQUARE_SIZE // 2 - self.PADDING
+        if not self.selected:
+            pygame.draw.circle(win, GREY, (self.x, self.y), radius + self.OUTLINE)
+        else:
+            pygame.draw.circle(win, GREEN, (self.x, self.y), radius + self.OUTLINE)
+        pygame.draw.circle(win, self.color, (self.x, self.y), radius)
+        if self.king:
+            win.blit(CROWN, (self.x - CROWN.get_width()//2, self.y - CROWN.get_height()//2))
+
+    def move(self, row, col):
+        self.row = row
+        self.col = col
+        self.calc_pos()
+
+    def __repr__(self):  # Representation
+        return str(self.color)
